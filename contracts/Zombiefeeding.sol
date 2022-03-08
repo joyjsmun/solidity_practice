2ad6da282ef00eba0a78010cfd1fd67c9308bbe0
joyjsmun <joy.js.mun@gmail.com>
2022-03-08 14:27:59 -0800

    ADD: adding kitty dna

--- contracts/Zombiefeeding.sol
+++ contracts/Zombiefeeding.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-// put import statement here
-import "./Zombiefactory.sol";
+import "./zombiefactory.sol";
 
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
@@ -19,11 +18,25 @@ contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public{
-        require(msg.sender == zombieToOwner[_zombieId]);
-        Zombie storage myZombie = zombies[_zombieId];
-        _targetDna = _targetDna % dnaModulus;
-        uint newDna = (myZombie.dna + _targetDna) /2;
-        _createZombie("NoName",newDna);
-    }
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  // Modify function definition here:
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Add an if statement here
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    // And modify function call here:
+    feedAndMultiply(_zombieId, kittyDna);
+  }
+
 }
