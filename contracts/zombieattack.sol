6fd553e30bf7b70d44c6c8524e2089e67cecd6e1
joyjsmun <joy.js.mun@gmail.com>
2022-03-13 15:47:58 -0700

    added win fn

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -4,8 +4,6 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
   uint randNonce = 0;
-  // Create attackVictoryProbability here
-
   uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
@@ -13,9 +11,18 @@ contract ZombieAttack is ZombieHelper {
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   }
 
-  // Create new function here
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    // Start here
+    if (rand <= attackVictoryProbability) {
+        myZombie.winCount++;
+        myZombie.level++;
+        enemyZombie.lossCount++;
+        feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
 
-  function attack(uint _zombieId, uint _targetId) external{
+        }
     
   }
 }
