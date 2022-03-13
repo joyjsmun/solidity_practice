991c4017dfcb4fae35745864d458278cb6801eea
joyjsmun <joy.js.mun@gmail.com>
2022-03-13 14:05:51 -0700

    added refactoring

--- contracts/Zombiefeeding.sol
+++ contracts/Zombiefeeding.sol
@@ -21,6 +21,13 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
+  // 1. Create modifier here
+modifier ownerOf(uint _zombieId) {
+  require(msg.sender == zombieToOwner[_zombieId]);
+  _;
+}
+
+
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
@@ -33,8 +40,9 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  // 2. Add modifier to function definition:
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+    // 3. Remove this line
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
@@ -51,5 +59,4 @@ contract ZombieFeeding is ZombieFactory {
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
-
 }
