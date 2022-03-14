34ecdfb50e316c1fedf940a9bcf00a2f1704e700
joyjsmun <joy.js.mun@gmail.com>
2022-03-14 15:23:40 -0700

    added more fn to transferFrom

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -5,6 +5,9 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  // 1. Define mapping here
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) external view returns (uint256) {
     return ownerZombieCount[_owner];
   }
@@ -13,20 +16,22 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     return zombieToOwner[_tokenId];
   }
 
-  // Define _transfer() here
-
   function _transfer(address _from, address _to, uint256 _tokenId) private {
     ownerZombieCount[_to]++;
     ownerZombieCount[_from]--;
     zombieToOwner[_tokenId] = _to;
-    emit Transfer(_from,_to,_tokenId);
+    emit Transfer(_from, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
+    // 2. Add the require statement here
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    // 3. Call _transfer
+    _transfer(_from, _to, _tokenId);
   }
 
   function approve(address _approved, uint256 _tokenId) external payable {
 
   }
+
 }
